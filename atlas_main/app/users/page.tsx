cf8e565555d6e3137a22d92196b14861653234c7
jhluc <jhluc@uci.edu>
2023-11-14 00:29:42 -0800

    added legend and scalebar

--- atlas_main/app/users/page.tsx
+++ atlas_main/app/users/page.tsx
@@ -3,16 +3,15 @@ import React from 'react'
 
 import MapComponent from '../../pages/api/biomes'
 const Userspage = () => {
-
+    
     return (
         <html>
             <head>
                 <link rel="stylesheet" href="https://js.arcgis.com/4.27/esri/themes/light/main.css"></link>
             </head>
-            <body >
-
+            
                 <MapComponent />
-            </body>
+            
         </html>
     )
 }
--- atlas_main/pages/api/biomes.tsx
+++ atlas_main/pages/api/biomes.tsx
@@ -14,11 +14,17 @@ import LayerView from "@arcgis/core/views/layers/LayerView.js";
 import Map from "@arcgis/core/Map.js";
 import { Main } from 'next/document';
 import { loadModules } from "esri-loader";
+import BasemapGallery from "@arcgis/core/widgets/BasemapGallery.js";
+import * as widget from "@arcgis/core/widgets/support/widget.js";
+import Legend from "@arcgis/core/widgets/Legend.js";
+import ScaleBar from "@arcgis/core/widgets/ScaleBar.js";
+
+
 const BIOMES_LAYER = "https://services.arcgis.com/BG6nSlhZSAWtExvp/ArcGIS/rest/services/GlobalBiomes/FeatureServer/0"
 const BIOMES_SERVICE = "https://services.arcgis.com/BG6nSlhZSAWtExvp/ArcGIS/rest/services/GlobalBiomes/FeatureServer"
 const BASE_MAP =  "https://server.arcgisonline.com/arcgis/rest/services/NatGeo_World_Map/MapServer/0"
  const MapComponent = () => {
-   
+    
     const loadMap = () => {
         console.log("not null")
         const map = new Map({
@@ -36,53 +42,51 @@ const BASE_MAP =  "https://server.arcgisonline.com/arcgis/rest/services/NatGeo_W
             url: BIOMES_LAYER
         });
 
+        view.constraints = {
+            minZoom: 2,
+            maxZoom: 6
+        };
+
         map.add(biomeslayer);
 
+        const legend = new Legend({
+            view: view
+            
+          });
+        const scaleBar = new ScaleBar({
+            view: view,
+            unit : "dual"
+          });
+          // Add widget to the bottom left corner of the view
+        view.ui.add(scaleBar, {
+            position: "bottom-left"
+          });
+        view.ui.add(legend, "bottom-right");
+        
+        // legend.when(legend.zo(){
+        //     // This function will execute once the promise is resolved
+        //   }, function(error){
+        //     // This function will execute if the promise is rejected due to an error
+        //   });
+        
+        
     }
 
     useEffect(() => {
         loadMap();
 
+
     }, []);
   
-    return  <div style={{ height : 1500, width: 2500 }} id='mapview'></div>
+    return  (
+        <body>
+            
+            <div id='mapview'></div>
+        </body>
+    )
 
 }
 
-// const fetch_feature_server = async () => {
-//     const layer = new FeatureLayer({
-//         // Notice that the url doesn't end with /2
-//         url : BIOMES_SERVICE
 
-//       });
-
-//     const myMap = new Map({
-//         basemap: "terrain"
-//     });
-
-//     const myMapView = new MapView({
-//         container: "viewDiv",
-//         map : myMap,
-       
-//     });
-
-
-//     let query = layer.createQuery();
-//     query.where = "1=1";
- 
-//     // layer.queryFeatures(query).then(function(results){
-//     //     // prints the array of result graphics to the console
-//     //     console.log(results.features);
-//     //   });
-//     //const res = await executeQueryJSON(BIOMES_LAYER)
-//     //construct a mapview object and perhaps export it and render it somehow 
-
-//     return (
-//         <main>
-           
-
-//         </main>
-//     )
-// }
 
 export default MapComponent;
