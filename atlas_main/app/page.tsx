4fc5874dfef127b17e241b22ba6f81054e800328
jhluc <jhluc@uci.edu>
2023-12-09 23:02:43 -0800

    no errors

--- atlas_main/app/page.tsx
+++ atlas_main/app/page.tsx
@@ -1,17 +1,19 @@
-import Image from 'next/image'
-import Link from 'next/link'
-export default function Home() {
-  return (
-    <main>
-      <h1>Biomes interactive Map</h1>
-      <a href="https://server.arcgisonline.com/arcgis/rest/services/World_Physical_Map/MapServer/layers">base map--MapServer</a>
-      <br></br>
-      <a href="https://services.arcgis.com/BG6nSlhZSAWtExvp/ArcGIS/rest/services/GlobalBiomes/FeatureServer/layers">base map--ServerFeature</a>
-      <br></br>
-      <Link href="/users">RENDER MAP</Link>
-      <br></br>
+'use client';
+import React from 'react'
+import dynamic from 'next/dynamic'
 
-    </main>
-    
-  )
+const Userspage = () => {
+    const EsriMapWithNoSSR = dynamic(() => import('./components/biomes'), {
+        ssr: false,
+      });
+      
+      
+    return (
+        
+        <EsriMapWithNoSSR />
+        
+        
+    )
 }
+
+export default Userspage
