b630cd49690ae8c4afdd2d0b1865f37b081dae2c
jhluc <jhluc@uci.edu>
2023-12-25 09:25:12 -0800

    lat long feature

--- atlas_main/app/components/biomes.tsx
+++ atlas_main/app/components/biomes.tsx
@@ -1,7 +1,7 @@
 //1-add rainfall and temperature featurelayer. 
 //2 - perhaps change feature colors
 //3 - more webscraped data
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { useEffect } from 'react';
 import 'app/globals.css'
 
@@ -26,12 +26,18 @@ import CustomContent from "@arcgis/core/popup/content/CustomContent.js";
 
 import styles from './mapdiv.module.css'
 import content_switch from './content_switch'
+
+import styles_dropdown from './input_dropdown.module.css'
+
+import axios from 'axios';
 const BIOMES_LAYER = "https://services.arcgis.com/BG6nSlhZSAWtExvp/ArcGIS/rest/services/GlobalBiomes/FeatureServer/0"
 const BIOMES_SERVICE = "https://services.arcgis.com/BG6nSlhZSAWtExvp/ArcGIS/rest/services/GlobalBiomes/FeatureServer"
 const BASE_MAP =  "https://server.arcgisonline.com/arcgis/rest/services/NatGeo_World_Map/MapServer/0"
 const KOPPEN_LAYER = "https://services3.arcgis.com/0OPQIK59PJJqLK0A/arcgis/rest/services/other_climate_2007_koppen_geiger/FeatureServer/0"
 
   const MapComponent = () => {
+
+
     const mapref = useRef<HTMLInputElement>(null);
     const loadMap = () => {
        //console.log("not null")
@@ -167,29 +173,44 @@ const KOPPEN_LAYER = "https://services3.arcgis.com/0OPQIK59PJJqLK0A/arcgis/rest/
       view.ui.add(layer_expand, "top-left");
 
     // let btn_foc =  useRef<HTMLInputElement>(null);
-    var btn_foc = document.createElement('button');
-    const text = document.createTextNode("Focus Modality");
-    btn_foc.style.background = "white";
+    // var btn_foc = document.createElement('button');
+    // const text = document.createTextNode("Search by Coordinates");
+    // btn_foc.style.background = "white";
     // add the text node to the newly created div
-    btn_foc.appendChild(text);
+    // btn_foc.appendChild(text);
 
-    const button_foc_ref = document.querySelector("btn_foc");
+    // const button_foc_ref = document.querySelector("btn_foc");
 
-    document.body.insertBefore(btn_foc, button_foc_ref);
-
+    // document.body.insertBefore(btn_foc, button_foc_ref);
+    console.log(styles_dropdown.inputdropdown);
+    const Inputdropdown = document.getElementById("input-wrap")!;
+    const expand = new Expand({
+        content: Inputdropdown,
+        expanded: false
+    })
+    view.ui.add(expand, "top-right")
+    // view.when(() => {
+    //     view.ui.add(expand, "top-right")
+    // })
     //console.log("all groups ",all_groups);
 
     //console.log("exists");
-    if (btn_foc != null){
-        view.ui.add(btn_foc, "top-right");
-        //console.log("exists1");
-        btn_foc.addEventListener("click", async () => {
 
-        });
+    // if (btn_foc != null){
+    //     view.ui.add(btn_foc, "top-right");
+    //     //console.log("exists1");
+    //     btn_foc.addEventListener("click", async (evt) => {
+    //         console.log("clicked");
+    //         //handleClick();
+    //         view.ui.add(expand, "top-right")
+
+    //     });
     
-    }
+    // }
 
-
+    view.when(() => {
+        view.ui.add(expand, "top-right")
+      })
 
     }
 
@@ -202,14 +223,80 @@ const KOPPEN_LAYER = "https://services3.arcgis.com/0OPQIK59PJJqLK0A/arcgis/rest/
 
         
     }, [mapref]);
+//Inputdropdown
+
+    function sendQuery() {
+      
+
+    }
+
+    const[inputValx, setInputValx] = useState("");
+    const[inputValy, setInputValy] = useState("");
+    const[display, setDisplay] = useState("");
+    function handleInputChangex(e: React.FormEvent<HTMLInputElement>){
+        let inputvalue = e.currentTarget.value;
+        setInputValx(inputvalue);
+        setDisplay("")
+        
+    }
+    function handleInputChangey(e: React.FormEvent<HTMLInputElement>){
+        let inputvalue = e.currentTarget.value;
+        setInputValy(inputvalue);
+        console.log(inputvalue);
+        setDisplay("")
+        
+    }
+
+    const  handleSubmit =  async (event : React.FormEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        // console.log(inputValx.trim());
+        // console.log(inputValy.trim());
+        if (!(inputValx.trim() === "" || inputValy.trim()  === "")) {
+            console.log("Both are not empty");
+
+           
+            const res = await axios
+                .get('http://climateapi.scottpinkelman.com/api/v1/location/'+ inputValy.trim() +'/' +inputValx.trim())
+            console.log(res.data)
+            try{
+                if (res.data){
+                    console.log(res.data)
+                    console.log(res.data['return_values'][0]['koppen_geiger_zone'])
+                    console.log(res.data['return_values'][0]['zone_description'])
+                    setDisplay(res.data['return_values'][0]['koppen_geiger_zone'] + ': ' + res.data['return_values'][0]['zone_description']);
+                }
+            }catch(e ){
+                console.log("Your Coordinates are not valid")
+                setDisplay("Your Coordinates are not valid")
+            }
+
+            
+        }
+        }
+      
+    
 
     return  (
     
-            
-            <div ref = {mapref} id={styles.mapview}>
-                
+        <>
+            <div ref = {mapref} id={styles.mapview}></div>
+            <div id="input-wrap" onSubmit={handleSubmit}>
+                <form id={styles_dropdown.inputdropdown}>
+                    <p>Find Biome by Coordinates</p>
+                    <label>Latitude</label>
+                    <input type="text" name= "Latitude"  onChange={handleInputChangey}/>
+                    <label>Longitude</label>
+                    <input type="text" name= "Longitude" onChange={handleInputChangex}/>
+                    <button> Search </button>
+                    <br></br>
+                    {
+                        display? display : null
+                    }
+                </form>
+
             </div>
-           
+
+        </>
     )
 
 }
